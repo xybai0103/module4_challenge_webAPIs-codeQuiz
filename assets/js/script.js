6c21f8a30ebe26740fa497e1df8ebbcea2a9047d
xybai0103 <xueyin0103@gmail.com>
2023-02-20 08:27:45 -0600

    correct order of functions and statements and complete renderQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var main = document.querySelector('main');
 var title = document.querySelector('.title');
 var message = document.querySelector('.message');
 var singleButton = document.querySelector('.single-button');
@@ -11,51 +12,37 @@ function init() {
   timer.textContent = 'Time: 0'
 }
 init();
-// timer starts from 75 seconds
-var secondsLeft = 75;
-// add a container to hold four choices of the single-choice question
-var answerChoices = document.createElement('div');
-answerChoices.classList.add('answerChoices');
-// add 4 label elements wrapping 4 input elements repesenting four choices
-var label = document.createElement('label');
-var input = document.createElement('input');
-input.type = 'radio';
-input.name = 'choice';
-input.setAttribute('hidden', '');
-// set an array of five question objects
-var questions = [question1, question2, question3, question4, question5];
+
+
 // title and answer properties of object question1
 var question1 = {
   title: 'Which of the following is an inline HTML element?',
   choices: ['1. div', '2. p', '3. h1', '4. img']
-}
-question1.choices[3].dataset.state = 'right';
-// title and answer properties of set object question2
+};
 var question2 = {
   title: 'Which CSS property is used to set the direction of flex items in a flex container?',
   choices: ['1. justify-conten', '2. flex-direction', '3. align-items', '4. flex-wrap']
-}
-question2.choices[1].dataset.state = 'right';
-// title and answer properties of set object question3
+};
 var question3 = {
   title: 'Which of the following is not a way to create a new object in JavaScript?',
   choices: ['1. Object constructor', '2. Object literal notation', '3. Object.create method', '4. Object.assign method']
-}
-question3.choices[2].dataset.state = 'right';
-// title and answer properties of set object question4
+};
 var question4 = {
   title: 'Which of the following is not a way to create a new object in JavaScript?',
   choices: ['1. removeListener', '2. off', '3. removeEventListener', '4. stopListening']
-}
-question4.choices[2].dataset.state = 'right';
-// title and answer properties of set object question5
+};
 var question5 = {
   title: 'Which method is used to retrieve data from local storage in JavaScript?',
   choices: ['1. localStorage.getItem()', '2. localStorage.setValue()', '3. localStorage.setItem()', '4. localStorage.getData()']
-}
-question5.choices[0].dataset.state = 'right';
+};
+// set an array of five question objects  **must initialize after setting all the objects
+var questions = [question1, question2, question3, question4, question5];
+// set an array of correct answers
+var correctAnswers = [question1.choices[3], question2.choices[1], question3.choices[2], question4.choices[2], question5.choices[0]];
 
 
+// timer starts from 75 seconds
+var secondsLeft = 75;
 // countDown function
 function countDown() {
     var timerInterval = setInterval(function() {
@@ -68,26 +55,41 @@ function countDown() {
         } 
       }, 1000);
 } 
+
+
+// create a container to hold four choices of the single-choice question
+var answerChoices = document.createElement('div');
+answerChoices.classList.add('answerChoices');
+
+
 // renderQuestion function
 function renderQuestion() {
+  main.append(answerChoices);
   for(i=0; i<4; i++){
+    // creat label elements wrapping 4 input elements repesenting four choices  **can not creat one tag and append multiple times
+    var label = document.createElement('label');
+    var input = document.createElement('input');
+      input.type = 'radio';
+      input.name = 'choice';
+      input.setAttribute('hidden', '');
     label.append(input);
-    choices.append(label);
-    label[i].textContent = questions[0].choices[i];
-  }
+    answerChoices.append(label);
+    answerChoices.children[i].textContent = questions[0].choices[i];
+  };
   title.textContent = questions[0].title;
 }
+
+console.log(answerChoices);
 // when click start button:
 singleButton.addEventListener('click', function(){
     // timer (countDown function) starts from 75 seconds
     countDown();
+    // clear message and single button
+    message.style.display = 'none';
+    singleButton.style.display = 'none';
     // present 1st single-choice question with 4 answer buttons (selection div?)
     renderQuestion();
-    // hide message and single button
-    message.style.visibility = 'hidden';
-    singleButton.style.visibility = 'hidden';
-}
-)
+});
 
 // when user clicks one of the 4 answer buttons of 1st question:
   // present 2nd single-choice question with 4 answer buttons
