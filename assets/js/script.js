d1181cea21b875e7516ad91ed8be5a59df85a6b3
xybai0103 <xueyin0103@gmail.com>
2023-02-21 13:04:31 -0600

    initialize function at the enter-initials page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 var main = document.querySelector('main');
 var title = document.querySelector('.title');
 var message = document.querySelector('.message');
-var singleButton = document.querySelector('.single-button');
+var startButton = document.querySelector('.start-button');
 var timer = document.querySelector('#timer');
+var currentQuestion = 0;
+// number of questions that user selects the correct answer
+var numberOfCorrect = 0;
+
+
 // application first-loading page has a "Coding Quiz Challenge" title, a start button, and a timer set to 0
 // The init() function fires when the page is loaded 
 function init() {
   title.textContent = 'Coding Quiz Challenge';
   message.textContent = 'Try to answer the following code-related questions with the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!';
-  singleButton.textContent = 'Start Quiz';
+  startButton.textContent = 'Start Quiz';
   timer.textContent = 'Time: 0'
 }
 init();
@@ -21,14 +26,14 @@ var question1 = {
 };
 var question2 = {
   title: 'Which CSS property is used to set the direction of flex items in a flex container?',
-  choices: ['1. justify-conten', '2. flex-direction', '3. align-items', '4. flex-wrap']
+  choices: ['1. justify-content', '2. flex-direction', '3. align-items', '4. flex-wrap']
 };
 var question3 = {
-  title: 'Which of the following is not a way to create a new object in JavaScript?',
+  title: 'Which of the following is the way to create a new object in JavaScript?',
   choices: ['1. Object constructor', '2. Object literal notation', '3. Object.create method', '4. Object.assign method']
 };
 var question4 = {
-  title: 'Which of the following is not a way to create a new object in JavaScript?',
+  title: 'Which of the following is the way to remove an event listener in JavaScript?',
   choices: ['1. removeListener', '2. off', '3. removeEventListener', '4. stopListening']
 };
 var question5 = {
@@ -43,17 +48,19 @@ var correctAnswers = [question1.choices[3], question2.choices[1], question3.choi
 
 // timer starts from 75 seconds
 var secondsLeft = 75;
+// timerInterval
+var timerInterval;
 // countDown function
 function countDown() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = "Time: " + secondsLeft;
-    
-        if(secondsLeft === 0) {
-          // Stops execution of action at set interval
-          clearInterval(timerInterval);
-        } 
-      }, 1000);
+  timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    } 
+  }, 1000);
 } 
 
 
@@ -62,44 +69,45 @@ var answerChoices = document.createElement('div');
 answerChoices.classList.add('answerChoices');
 
 
-// renderQuestion function
+// addAnswerButtons function
 function addAnswerButtons() {
   main.append(answerChoices);
   for(i=0; i<4; i++){
     // creat label elements wrapping 4 input elements repesenting four choices  **can not creat one tag and append multiple times
-    var label = document.createElement('label');
+    var answerLabel = document.createElement('label');
+    answerLabel.classList.add('answer-label');
     var input = document.createElement('input');
       input.type = 'radio';
       input.name = 'choice';
       input.setAttribute('hidden', '');
-    label.append(input);
-    answerChoices.append(label);
+    answerLabel.append(input);
+    answerChoices.append(answerLabel);
   };
 }
 
+// function to render a question
 function renderQuestion() {
   for(j=0; j<4; j++){
-    answerChoices.children[j].textContent = questions[0].choices[j];
+    answerChoices.children[j].textContent = questions[currentQuestion].choices[j];
   };
-  title.textContent = questions[0].title;
+  title.textContent = questions[currentQuestion].title;
 }
-  
-
 
 
 // when click start button:
-singleButton.addEventListener('click', function(){
+startButton.addEventListener('click', function(){
     // timer (countDown function) starts from 75 seconds
     countDown();
     // clear message and single button
     message.style.display = 'none';
-    singleButton.style.display = 'none';
-    // present 1st single-choice question with 4 answer buttons
+    startButton.style.display = 'none';
+    // present 1st single-choice question
     addAnswerButtons();
     renderQuestion();
 });
 
-// when user clicks one of the 4 answer buttons of 1st question:
+
+// when user clicks one of the 4 answer buttons of a question:
 answerChoices.addEventListener('click', function(event){
     var userChoice = event.target;
     // create a container for an alert message and a horizontal line
@@ -109,9 +117,10 @@ answerChoices.addEventListener('click', function(event){
     var hr = document.createElement('hr');
     messageContainer.append(hr, alertMessage);
     main.append(messageContainer);
-    // if for 1st question user clicked the correct answer button, display "Correct!" for a second
+    // if for user clicked the correct answer button, display "Correct!" for a second
     if (correctAnswers.includes(userChoice.textContent)){
        alertMessage.textContent = 'Correct!';
+       numberOfCorrect++;
     }
     // else:
     else {
@@ -124,28 +133,49 @@ answerChoices.addEventListener('click', function(event){
     setTimeout(() => {
     messageContainer.style.display = "none";
     }, 1000);
+    
+    // present next question
+    function renderNextQuestion() {
+      currentQuestion++;
+      if (currentQuestion < 5) {
+        renderQuestion();
+      // when user answers the 5th question:
+      } else {
+        // stop timer
+        clearInterval(timerInterval);
+        // set timer to 0
+        timer.textContent = "Time: 0";
+        var enterInitials = function(){
+          // display "All done!"
+          title.textContent = 'All done!';
+          // clear answer buttons
+          answerChoices.style.display = 'none';
+          // calculate user's score: number of correct answers plus number of seconds left when timer stops
+          var score = numberOfCorrect + secondsLeft;
+          // display final score
+          message.style.display = '';
+          message.textContent = 'Your final score: ' + score;
+          // display an input area for user to input initials
+          var initialLabel = document.createElement('label');
+          initialLabel.textContent = 'Enter Initials: ';
+          var initialInput = document.createElement('input');
+          initialInput.type = 'text';
+          // display a submit button
+          var submitButton = document.createElement('input');
+          submitButton.type = 'submit';
+          submitButton.classList.add('submit-button');
+          // put the input area and submit button before alert message
+          main.insertBefore(initialLabel, messageContainer);
+          main.insertBefore(initialInput, messageContainer);
+          main.insertBefore(submitButton, messageContainer);
+        }
+        enterInitials();
+      }
+    }
+
+    renderNextQuestion();
 });
 
-
-  // present 2nd single-choice question with 4 answer buttons
-
-
-// when user clicks one of the 4 answer buttons of 2nd question:
-  // present 3rd single-choice question with 4 answer buttons
-  // if for 2nd question user clicked the correct answer button, display "Correct!" for a second
-  // else:
-      // display "Wrong!" for a second
-      // 10 seconds substracted from timer
-// .. same process as above..
-// when user clicks one of the 4 answer buttons of 5th question:
-  // timer stops
-  // display "All done!" + final score
-  // display an input area for user to input initials
-  // display a submit button
-  // if for 5th question user clicked the correct answer button, display "correct!" for a second
-  // else:
-      // display "wrong!" for a second
-      // 10 seconds substracted from timer
   
 // when timer reaches 0:
   // timer stops
@@ -163,11 +193,8 @@ answerChoices.addEventListener('click', function(event){
     // when user clicks the button, all "initial -final score"s list items cleared
   // clear timer
 
-// how to calculate user's score:
-  // number of correct answers (0=< x <=5) plus number of seconds left when timer stops
+
 
 //TASKS
-  // use timers-Intervals to countdown
   // store "initial -final score" in localStorage: convert object to JSON string
     // pull it out to use: turn JSON string back to object
-  // each question is an object with topic and answer properties, put objects in a question array, and loop through each object in the array
